960e322f71b70935b99861c9133c24b166d86ff0
Vitalii <vitalii.ilchenko@gmail.com>
2018-12-07 16:56:47 +0200

    faza

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,20 @@
-$(function(){
-    $('.today-navigation a').click(function(){
-        $(this).parents('.today-tabs').find('.today-wrap').addClass('hide');
-        $(this).parent().siblings().removeClass('active');
-        var id = $(this).attr('href');
-        $(id).removeClass('hide');
-        $(this).parent().addClass('active');
-        return false
-    });
-});
+// $(function(){
+//     $('.today-navigation a').click(function(){
+//         $(this).parents('.today-tabs').find('.today-wrap').addClass('hide');
+//         $(this).parent().siblings().removeClass('active');
+//         var id = $(this).attr('href');
+//         $(id).removeClass('hide');
+//         $(this).parent().addClass('active');
+//         return false
+//     });
+// });
+
+
+var tab = document.querySelectorAll('.today-wrap');
+
+var navBtn = document.querySelectorAll('.today-navigation__item');
+for (var i = 0; i < navBtn.length; i++){
+    navBtn.onclick = function () {
+        console.log('asdasd');
+    }
+}
